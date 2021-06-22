12aad72f636d92ae32aa9879e43fb761cf902fc5
Emmanuel Trujillo Castillo <emmanuel1998@hotmail.com>
2021-06-22 00:25:54 -0700

    S

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Puto
+          Edit <code>src/App.js</code> and save to reload. Putos
         </p>
         <a
           className="App-link"
