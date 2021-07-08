37b63b4c185d3488261b04b1e4c3d0a16462c747
EmmanuelTC <emmanuel1998@hotmail.com>
2021-07-08 14:30:17 -0700

    Se agrego el inicio para el Curriculum

--- src/components/Screens/Curriculum.js
+++ src/components/Screens/Curriculum.js
@@ -1,92 +1,96 @@
 import React from 'react'
-//import Badge from '@material-ui/core/Badge'
-//import { makeStyles, withStyles } from '@material-ui/core/styles'
-//import { Avatar/*, Box*/ } from '@material-ui/core';
-/*import { TwitchChat, TwitchPlayer } from 'react-twitch-embed';*/
-
-
-/*const StyledBadge = withStyles((theme) => ({
-    badge: {
-        backgroundColor: '#ff0000',
-        color: '#ff0000',
-        boxShadow: `0 0 0 7px #990000`,
-        '&::after': {
-            position: 'absolute',
-            top: -1,
-            left: -1,
-            width: '100%',
-            height: '100%',
-            borderRadius: '50%',
-            animation: '$ripple 1.2s infinite ease-in-out',
-            border: '1px solid currentColor',
-            content: '""',
-        },
-    },
-    '@keyframes ripple': {
-        '100%': {
-            transform: 'scale(4.8)',
-            opacity: 0,
-        },
-        '10%': {
-            transform: 'scale(2.4)',
-            opacity: 1,
-        },
-    },
-}))(Badge);
+import { makeStyles } from '@material-ui/core/styles'
+//import './CurriculumStyles.css';
+import { Typography, Container, Box } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        '& > *': {
-            margin: theme.spacing(1),
-        },
+    hr: {
+        height: "1px",
+        borderWidth: "1px 0px 1px 0px",
+        borderColor: "#ffffff",
+        borderStyle: "solid",
     },
-    AvatarSize: {
-        width: '100px',
-        height: '100px'
-    }
-}));
-
-const SmallAvatar = withStyles((theme) => ({
-    root: {
-        width: 40,
-        height: 40,
-        border: `2px solid ${theme.palette.background.paper}`,
+    NombreCurriculum: {
+        backgroundColor: "#000000",
+        margin: "0px",
+        color: "#000000",
+        fontSize: "40px"
     },
-    badgeOffline: {
-        backgroundColor: '#cccccc',
-        color: '#cccccc',
-        boxShadow: `0 0 0 7px #999999`,
-        '&::after': {
-            position: 'absolute',
-            top: -1,
-            left: -1,
-            width: '100%',
-            height: '100%',
-            borderRadius: '50%',
-            animation: '$ripple 1.2s infinite ease-in-out',
-            border: '1px solid currentColor',
-            content: '""',
-        },
+    TextoBordeBlanco: {
+        margin: "0px",
+        textShadow: "2px 0 0 #fff, -2px 0 0 #fff, 0 2px 0 #fff, 0 -2px 0 #fff, 1px 1px #fff, -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff"
+    },
+    TextoBlanco: {
+        color: "#ffffff",
+        fontSize: "30px",
+    },
+    TextoNegritas: {
+        fontWeight: "bold",
+    },
+    Dentro: {
+        width: "100px", height: "100px",
+        position: "relative", float: "left",
+        backgroundColor: "#ffffff",
+        margin: "0px 5px",
+        color: "#000000",
+        fontWeight: "bold"
+    },
+    TituloHabilidades:{
+        fontSize: "25px"
+    },
+    PuntosHabilidades:{
+        fontSize:"15px"
     }
-}))(Avatar);*/
+}))
 
 const Curriculum = () => {
-//    const classes = useStyles();
+    const classes = useStyles();
     return (
         <div>
             <div style={{ height: "10px", backgroundColor: "#ffffff" }} />
             <h1><code>Curriculum</code></h1>
-{/*            <hr />
-            <div>
-                <StyledBadge overlap="circle" anchorOrigin={{ vertical: 'bottom', horizontal: 'right', }} variant="dot" >
-                    <Badge overlap="circle" anchorOrigin={{ vertical: 'top', horizontal: 'left', }} badgeContent={<SmallAvatar alt="NorsuisoSquad" src="https://avatars.githubusercontent.com/u/81671829" />} >
-                        <Avatar className={classes.AvatarSize} alt="EmmanuelTC" src="https://static-cdn.jtvnw.net/jtv_user_pictures/04fc7ed0-e995-4d2a-82c0-e9bbc2b48dc2-profile_image-300x300.png" />
-                    </Badge>
-                </StyledBadge>
 
-                <br /><br />
-            </div>*/}
+            <div style={{ backgroundColor: "#000000" }}>
+                <div className={classes.NombreCurriculum}>
+                    <span className={classes.TextoBordeBlanco}>EMMANUEL TRUJILLO CASTILLO</span>
+                </div>
+                <div className={classes.hr}></div>
+                <div>{/* Contenido de Curriculum */}
+                    <div style={{ backgroundColor: "#000000", margin: "0px" }}>
+                        <span className={`${classes.TextoBlanco} ${classes.TextoNegritas}`}>PERFIL</span><br />
+                        <span className={`${classes.TextoBlanco}`} style={{ fontSize: "15px" }}>
+                            Mi nombre es Emmanuel, nacido en Hermosillo, Sonora, México, estudie
+                            Ingeniería Informática, me gusta aprender de los demás y las novedades
+                            para reforzar e innovar.
+                        </span>
+                    </div><br />
+                    <div style={{ backgroundColor: "#000000", margin: "0px" }}>
+                        <span className={`${classes.TextoBlanco} ${classes.TextoNegritas}`}>HABILIDADES</span><br />
+                        <Container style={{ padding: "10px", color: "#ffffff", fontSize: "20px" }} className={classes.footer} maxWidth="max" position="end">
+                            <Box style={{ paddingBottom: "10px" }} display="flex" >
+                                <Box flexGrow={1} className={classes.Dentro}>
+                                    <Typography className={`${classes.TituloHabilidades} ${classes.TextoNegritas}`}>SOFT</Typography>
+                                    <Typography className={classes.PuntosHabilidades}>Punto 1</Typography>
+                                </Box>
+                                <Box flexGrow={1} className={classes.Dentro}>
+                                <Typography className={`${classes.TituloHabilidades} ${classes.TextoNegritas}`}>HARD</Typography>
+                                    <Typography className={classes.PuntosHabilidades}>Punto 1</Typography>
+                                </Box>
+                            </Box>
+                            <Box display="flex" >
+                                <Box flexGrow={1} className={classes.Dentro}>
+                                <Typography className={`${classes.TituloHabilidades} ${classes.TextoNegritas}`}>TOOLS</Typography>
+                                    <Typography className={classes.PuntosHabilidades}>Punto 1</Typography>
+                                </Box>
+                                <Box flexGrow={1} className={classes.Dentro}>
+                                <Typography className={`${classes.TituloHabilidades} ${classes.TextoNegritas}`}>FRAMEWORK</Typography>
+                                    <Typography className={classes.PuntosHabilidades}>Punto 1</Typography>
+                                </Box>
+                            </Box>
+                        </Container>
+                    </div>
+                </div>{/* Termina Contenido de Curriculum */}
+            </div>
         </div>
     )
 }
